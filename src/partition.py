198b3264e64c1f5e909e131d950e33d42f1193a8
Yiming Zhang <yimingz3@cs.cmu.edu>
2025-05-19 14:54:28 -0700

    fix HF classifier name

--- src/partition.py
+++ src/partition.py
@@ -31,7 +31,7 @@ def load_deberta_tokenizer_and_model():
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
     tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v3-large")
     model = AutoModelForSequenceClassification.from_pretrained(
-        "deberta-v3-large-generation-similarity"
+        "yimingzhang/deberta-v3-large-generation-similarity"
     ).to(DEVICE)
     model.eval()
     return tokenizer, model
